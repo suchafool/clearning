25d9a7a333d301f266e5c79233de8f4f0791d6b5
sunht <beyondsht@126.com>
2018-06-06 16:47:09 +0800

    add test11.c

---
+++ c/test11.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  float b;
+  char c;
+  float d;
+
+  scanf("%d", &a);
+  printf("a=%d \n", a);
+
+  scanf("%10f", &b);
+  printf("b=%f\n", b);
+
+  scanf("%s", &c);
+  printf("c=%c\n", c);
+
+  //scanf("%d", &d);
+  printf("d=%f\n", d);
+
+}
